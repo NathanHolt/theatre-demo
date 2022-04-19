14ed10b13066b4ed62e16324ba2ff70b756be4fd
Nathan Holt <nateofakin@gmail.com>
2022-04-19 10:19:50 -0400

    Added a switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,8 @@ public class Main {
         NumberFormat numFor = NumberFormat.getCurrencyInstance();
         //create age int
         int age;
-        double price = 12.5;
-        boolean hasToy = false;
-        boolean hasSticker = false;
+        double price;
+        String extra = "";
 
         System.out.println("Please input your age");
         age = scanner.nextInt();
@@ -21,24 +20,44 @@ public class Main {
 
             // nested if, age 5 and under
             if (age < 5) {
-                hasToy = true;
+                extra = "toy";
             }
         } else if (age > 65) {
             price = 8;
 
             if (age >= 70) {
-                hasSticker = true;
+                extra = "sticker";
             }
+            if (age >= 80) {
+                extra = "advil";
+            }
+        } else {
+            price = 12.5;
         }
 
 
         //output the amount owed (money = double)
         System.out.println("You owe this much: " + numFor.format(price));
         //print out if they got a toy
-        if (hasToy) {
-            System.out.println("And here is a free toy!");
-        } else if (hasSticker) {
-            System.out.println("And here is a free sticker!");
+
+        switch (extra) {
+            case "toy":
+                System.out.println("You got a toy!");
+                break;
+            case "sticker":
+                System.out.println("You got a sticker!");
+                break;
+            case "advil":
+                System.out.println("You got a advil!");
+                break;
+            default:
+                System.out.println("No extra.");
+                break;
         }
+//        if (hasToy) {
+//            System.out.println("And here is a free toy!");
+//        } else if (hasSticker) {
+//            System.out.println("And here is a free sticker!");
+//        }
     }
 }
